24992470abe0eba78426c8662ac9c1ccd2b4d7e5
Vazohh <vazohhbtw@gmail.com>
2023-11-06 16:01:51 +0100

    Base pour passer à une architecture MVC OO V2

--- db.sql
+++ db.sql
@@ -2,6 +2,10 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE DATABASE blog;
+
+USE blog;
+
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
